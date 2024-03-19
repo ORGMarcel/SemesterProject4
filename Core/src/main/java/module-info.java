e4efc07b46384e15e76d99f3645d4f4882ebf228
Oliver Larsen <Oliver1703dk@hotmail.com>
2024-03-19 13:55:50 +0100

    v0.0.0 - Project Setup

---
+++ Core/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Core {
+    requires Common;
+    requires javafx.graphics;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics;
+    uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+    uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+}
+
