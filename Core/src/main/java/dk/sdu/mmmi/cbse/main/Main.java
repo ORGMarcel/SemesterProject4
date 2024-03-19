e9f3168f072ca20ca650fd30368ef62728445eac
Oliver Larsen <Oliver1703dk@hotmail.com>
2024-03-19 14:35:20 +0100

    v0.0.0 - Project setup correction

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -38,9 +38,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage window) throws Exception {
-        Text text = new Text(10, 20, "Destroyed asteroids: 0");
+//        Text text = new Text(10, 20, "Destroyed asteroids: 0");
         gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
-        gameWindow.getChildren().add(text);
+//        gameWindow.getChildren().add(text);
 
         Scene scene = new Scene(gameWindow);
         scene.setOnKeyPressed(event -> {
@@ -91,7 +91,7 @@ public class Main extends Application {
         render();
 
         window.setScene(scene);
-        window.setTitle("ASTEROIDS");
+        window.setTitle("Shadow Shuriken");
         window.show();
 
     }
