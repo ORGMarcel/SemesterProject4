604c6b4d2e8530362774c90938c738d15bb1f94e
Oliver Larsen <Oliver1703dk@hotmail.com>
2024-04-22 13:27:33 +0200

    v0.1.3 - Added map collision with objects

---
+++ Map/src/main/java/dk/sdu/mmmi/mapsystem/MapControlSystem.java
@@ -0,0 +1,22 @@
+package dk.sdu.mmmi.mapsystem;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+public class MapControlSystem implements IEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
