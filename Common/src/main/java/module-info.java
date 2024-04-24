0542639a4102b506bfafcc299e51925ede075076
Oliver Larsen <Oliver1703dk@hotmail.com>
2024-04-24 15:08:12 +0200

    v0.0.1 - Small fixes works here

--- Common/src/main/java/module-info.java
+++ Common/src/main/java/module-info.java
@@ -2,5 +2,4 @@ module Common {
     exports dk.sdu.mmmi.cbse.common.services;
     exports dk.sdu.mmmi.cbse.common.data;
     exports dk.sdu.mmmi.cbse.common.data.entityparts;
-    exports dk.sdu.mmmi.cbse.common.data.entityparts.mapparts;
 }
--- CommonMap/src/main/java/dk/sdu/mmmi/cbse/commonmap/CommonMap.java
+++ CommonMap/src/main/java/dk/sdu/mmmi/cbse/commonmap/CommonMap.java
@@ -1,9 +1,19 @@
 package dk.sdu.mmmi.cbse.commonmap;
 
 import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.commonmapobject.CommonMapObject;
 
 
 public class CommonMap extends Entity {
 
+    private CommonMapObject[][] map;
 
+
+    public CommonMapObject[][] getMap() {
+        return map;
+    }
+
+    public void setMap(CommonMapObject[][] map) {
+        this.map = map;
+    }
 }
