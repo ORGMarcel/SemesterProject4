47a06d7f31c47ace7758886ced40d3c41cf9c60c
ORGMarcel <113438149+ORGMarcel@users.noreply.github.com>
2024-05-07 11:46:16 +0200

    v0.1.4 - Added correct junit version in weapon pom. Interface test now passes.

--- Weapon/src/test/java/dk/sdu/mmmi/cbse/weaponsystem/InterfaceTestWeapon.java
+++ Weapon/src/test/java/dk/sdu/mmmi/cbse/weaponsystem/InterfaceTestWeapon.java
@@ -1,16 +1,15 @@
 package dk.sdu.mmmi.cbse.weaponsystem;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.weaponsystem.WeaponControlSystem;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class InterfaceTestWeapon {
 
-        @Test
-        public void testWeaponImplementsInterface() {
-            WeaponControlSystem weaponControlSystem = new WeaponControlSystem();
-            assertSame(weaponControlSystem.getClass().getInterfaces()[0], IEntityProcessingService.class,
-                    "WeaponControlSystem should implement the interface IEntityProcessingService");
-        }
+    @Test
+    public void testWeaponImplementsInterface() {
+        WeaponControlSystem weaponControlSystem = new WeaponControlSystem();
+        assertSame(weaponControlSystem.getClass().getInterfaces()[0], IEntityProcessingService.class, "WeaponControlSystem should implement the interface IEntityProcessingService");
+    }
 }
