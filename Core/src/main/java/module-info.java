3a0f3c87a26e89cbec50af160215c68777e21068
Oliver Larsen <Oliver1703dk@hotmail.com>
2024-05-30 14:14:50 +0200

    Clean up - unused require removed

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -4,7 +4,6 @@ import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 module Core {
     requires Common;
     requires javafx.graphics;
-    requires CollisionSystem;
     opens dk.sdu.mmmi.cbse.main to javafx.graphics;
     uses IGamePluginService;
     uses IEntityProcessingService;
